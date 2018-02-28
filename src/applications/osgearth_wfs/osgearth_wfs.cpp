2dc81c450e86fc900429f51c58716fec6be20077
Jason Beverage <jasonbeverage@gmail.com>
2018-02-28 17:15:21 -0500

    Reverted bad commit to wfs example

--- src/applications/osgearth_wfs/osgearth_wfs.cpp
+++ src/applications/osgearth_wfs/osgearth_wfs.cpp
@@ -20,163 +20,107 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
-#include <osgEarth/XmlUtils>
-#include <osgEarth/TileKey>
-#include <osgEarth/Registry>
-#include <osgDB/ReadFile>
-#include <osgDB/WriteFile>
-#include <osgDB/FileNameUtils>
-#include <osg/Math>
+#include <osgViewer/Viewer>
+#include <osgEarth/Notify>
+#include <osgEarthUtil/EarthManipulator>
+#include <osgEarthUtil/ExampleResources>
+
+#include <osgEarth/Units>
+#include <osgEarth/MapNode>
+
+// include for WFS feature driver:
+#include <osgEarthDrivers/feature_wfs/WFSFeatureOptions>
+
+// include for feature geometry model renderer:
+#include <osgEarthAnnotation/FeatureNode>
+#include <osgEarthSymbology/Style>
+
+
+#define LC "[wfs example] "
 
 using namespace osgEarth;
+using namespace osgEarth::Annotation;
+using namespace osgEarth::Util;
+using namespace osgEarth::Features;
+using namespace osgEarth::Symbology;
 
-#define INDEX_HEADER_SIZE 16
-#define INDEX_SIZE 5
-
-unsigned int  computeOffset(const std::vector<char>& buffer) {
-    unsigned int sum = 0;
-    for (unsigned int i = 0; i < buffer.size(); i++) {
-        char v = buffer[i];
-        //std::cout << (unsigned int)v << std::endl;
-        sum += ((unsigned int)v & 0xff) * pow(2, 8 * i);
-    }
-    return sum;
-}
-
-class BundleReader
+int
+usage(const char* name)
 {
-public:
-    BundleReader(const std::string& directory):
-        _directory(directory),
-        _bundleDim(128) // This is in the conf but I think is the same for all of them.
-    {    
-    }
+    OE_NOTICE 
+        << "\nUsage: " << name << " file.earth" << std::endl
+        << MapNodeHelper().usage() << std::endl;
 
-    /**
-     * Reads the index of a buffer file.
-     */
-    void readIndex(const std::string& filename, std::vector<int>& index)
-    {        
-        std::ifstream input(filename.c_str(), std::ifstream::binary);
-        char header[INDEX_HEADER_SIZE];
-        input.read(header, INDEX_HEADER_SIZE);
-        while (input.good()) {
-            std::vector<char> buffer;
-            buffer.resize(5);
-            if (input.read(&buffer[0], INDEX_SIZE))
-            {
-                int offset = computeOffset(buffer);
-                index.push_back(offset);
-            }
-        }
-    }
-
-    void readTile(std::istream& buffer, int offset, std::ostream& output)
-    {        
-        OSG_NOTICE << "Offset " << offset << std::endl;
-        buffer.seekg(offset, std::ios::beg);
-        std::vector<char> sizeBuffer;
-        sizeBuffer.resize(4);
-        buffer.read(&sizeBuffer[0], 4);
-        int size = computeOffset(sizeBuffer);
-        if (size > 0)
-        {
-            OSG_NOTICE << "Size=" << size << std::endl;
-
-            char* image = new char[size];
-            buffer.read(image, size);
-            output.write(image, size);
-            delete[] image;
-        }
-        else
-        {
-            OSG_NOTICE << "Size is 0" << std::endl;
-        }
-    }
-
-    unsigned int hexFromString(const std::string& input)
-    {
-        unsigned int result;
-        std::stringstream ss;
-        ss << std::hex << input;
-        ss >> result;
-        return result;
-    }
-
-    osg::Image* getImage(TileKey &key)
-    {
-        std::string base = "D:/geodata/simdis/arcgis_tile_package/Midnight_Canvas_4326/CanvasMaps_Midnight/Layers/_alllayers/L08/R0080C0180";
-        //std::string base = "D:/geodata/simdis/arcgis_tile_package/Midnight_Canvas_4326/CanvasMaps_Midnight/Layers/_alllayers/L08/RffffCffff";
-        std::string bundleFile = base + ".bundle";
-        std::string indexFile = base + ".bundlx";
-        
-        readIndex(indexFile, _index);
-        OSG_NOTICE << "Read " << _index.size() << " indices" << std::endl;
-
-        std::string baseName = osgDB::getNameLessExtension(osgDB::getSimpleFileName(bundleFile));
-        std::cout << "Basename=" << baseName << std::endl;
-
-        /*
-        unsigned int colOffset, rowOffset;       
-        std::stringstream ss;
-        ss << std::hex << baseName.substr(1, 4);
-        std::cout << "Row=" << ss.str();
-        ss >> rowOffset;
-
-        ss.str("");
-        ss.clear();
-        ss << std::hex << baseName.substr(6, 4);
-        std::cout << "Col=" << ss.str();
-        ss >> colOffset;
-        */
-        unsigned int rowOffset = hexFromString(baseName.substr(1, 4));
-        unsigned int colOffset = hexFromString(baseName.substr(6, 4));
-
-        std::cout << "row=" << rowOffset << ", col=" << colOffset << std::endl;
-
-
-
-        std::ifstream bundle(bundleFile.c_str(), std::ofstream::binary);
-
-        for (unsigned int i = 0; i < _index.size(); i++)
-        {
-            unsigned int row = floor(float(i) / _bundleDim);                
-            unsigned int x = colOffset + row;
-            unsigned int y = rowOffset + i - (row * _bundleDim);
-
-            std::cout << "Writing index " << i << " (" << x << ", " << y << ")" << std::endl;
-            std::stringstream outName;
-            outName << i << ".png";
-
-            std::stringstream buf;
-            readTile(bundle, _index[i], buf);
-            if (buf.str().size() > 0)
-            {
-                std::string data = buf.str();
-                std::ofstream out(outName.str().c_str(), std::ofstream::binary);
-                out.write(data.c_str(), data.size());
-            }
-        }
-
-        return 0;       
-    }
-
-    int _bundleDim;
-
-    std::string _directory;
-    std::vector< int > _index;
-    int _indexSize;
-};
+    return 0;
+}
 
 int
 main(int argc, char** argv)
 {
-    BundleReader reader("D:/geodata/simdis/arcgis_tile_package/Midnight_Canvas_4326/CanvasMaps_Midnight/Layers");
+    osg::ArgumentParser arguments(&argc,argv);
+    if ( arguments.read("--help") )
+        return usage(argv[0]);
 
-    const Profile* profile = osgEarth::Registry::instance()->getGlobalGeodeticProfile();
-    
-    osg::Image* image = reader.getImage(TileKey(0, 0, 0, profile));
-    if (image) {
-        osgDB::writeImageFile(*image, "image.png");
+    // general setup:
+    osgViewer::Viewer viewer(arguments);
+    viewer.getDatabasePager()->setUnrefImageDataAfterApplyPolicy( false, false );
+    viewer.setCameraManipulator( new EarthManipulator(arguments) );
+    viewer.getCamera()->setSmallFeatureCullingPixelSize(-1.0f);
+    viewer.getCamera()->setNearFarRatio(0.00002);
+
+    // Get the bounds from the command line.
+    Bounds bounds;
+    double xmin = DBL_MAX, ymin = DBL_MAX, xmax = DBL_MIN, ymax = DBL_MIN;
+    while (arguments.read("--bounds", xmin, ymin, xmax, ymax))
+    {
+        bounds.xMin() = xmin, bounds.yMin() = ymin, bounds.xMax() = xmax, bounds.yMax() = ymax;
+    }        
+
+    // load an earth file, and support all or our example command-line options
+    // and earth file <external> tags    
+    osg::Node* node = MapNodeHelper().load( arguments, &viewer );
+    if ( node )
+    {        
+        MapNode* mapNode = MapNode::get(node);
+        if ( !mapNode )
+            return usage(argv[0]);
+
+        // Create the WFS driver:
+        osgEarth::Drivers::WFSFeatureOptions wfs;
+        wfs.url()          = osgEarth::URI("http://demo.mapserver.org/cgi-bin/wfs"); 
+        wfs.typeName()     = "cities"; 
+        wfs.outputFormat() = "gml2";     // JSON or GML
+
+        // Create the feature source from the options
+        osg::ref_ptr< FeatureSource > featureSource = FeatureSourceFactory::create(wfs);
+        Status s = featureSource->open();
+
+        // Set the query with the bounds if one was specified.
+        Query query;
+        if (bounds.isValid())
+        {
+            query.bounds() = bounds;
+        }
+
+        // Get the features
+        osg::ref_ptr< FeatureCursor > cursor = featureSource->createFeatureCursor(query);
+        FeatureList features;
+        cursor->fill(features);
+        OE_NOTICE << "Got " << features.size() << " features" << std::endl;
+
+        // Create a style
+        Style style;
+        style.getOrCreateSymbol<TextSymbol>()->content() = StringExpression("[NAME]");
+
+        // Create the FeatureNode with the features and the style.
+        osg::ref_ptr< FeatureNode > featureNode = new FeatureNode(mapNode, features, style);
+        mapNode->addChild(featureNode.get());                
+
+        viewer.setSceneData( node );
+        return viewer.run();
+    }
+    else
+    {
+        return usage(argv[0]);
     }
 }
